6835b24d8fa2cd17db2ca48a65149505c44cbf07
Austin Graham <austiingraham@gmail.com>
2023-12-18 19:21:18 -0500

    establish and export MySQL database connection

--- js/database.js
+++ js/database.js
@@ -7,3 +7,14 @@ const connection = mysql.createConnection({
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
 });
+
+connection.connect((err) => {
+  if (err) {
+    console.error('\x1b[31m Error connecting to database:\x1b[0m', err.message);
+    return;
+  }
+
+  console.log('\x1b[32m Connected to the database\x1b[0m');
+});
+
+module.exports = connection;
