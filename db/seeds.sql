036c59cee565e1be07b39372481abef42c684407
Austin Graham <austiingraham@gmail.com>
2023-12-20 05:38:11 -0500

    added initial employee data to database

--- db/seeds.sql
+++ db/seeds.sql
@@ -12,3 +12,15 @@ VALUES (1, 'Software Engineer', 80000, 1),
        (4, 'Marketing Coordinator', 70000, 4),
        (5, 'Financial Analyst', 85000, 5),
        (6, 'Quality Assurance Engineer', 82000, 1);
+
+INSERT INTO employee (id, first_name, last_name, role_id, manager_id) 
+VALUES (1, 'John', 'Doe', 1, NULL),
+       (2, 'Jane', 'Smith', 2, NULL),
+       (3, 'Bob', 'Johnson', 3, 2),
+       (4, 'Alice', 'Williams', 1, 1),
+       (5, 'Charlie', 'Brown', 3, 2),
+       (6, 'Eva', 'Lee', 4, NULL),
+       (7, 'Michael', 'Clark', 5, NULL),
+       (8, 'Grace', 'Miller', 6, 1),
+       (9, 'David', 'Wong', 1, 1),
+       (10, 'Sophie', 'Baker', 4, 7);
