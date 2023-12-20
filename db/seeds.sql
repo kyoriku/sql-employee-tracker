3281688499732cd931b67aaf7128e606c4d33c8a
Austin Graham <austiingraham@gmail.com>
2023-12-20 04:46:27 -0500

    added initial role data to database

--- db/seeds.sql
+++ db/seeds.sql
@@ -4,3 +4,11 @@ VALUES (1, 'Engineering'),
        (3, 'Human Resources'),
        (4, 'Marketing'),
        (5, 'Finance');
+
+INSERT INTO role (id, title, salary, department_id) 
+VALUES (1, 'Software Engineer', 80000, 1),
+       (2, 'Sales Manager', 90000, 2),
+       (3, 'HR Specialist', 75000, 3),
+       (4, 'Marketing Coordinator', 70000, 4),
+       (5, 'Financial Analyst', 85000, 5),
+       (6, 'Quality Assurance Engineer', 82000, 1);
