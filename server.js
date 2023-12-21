c2d698aea1f7b0c1da9ecc9799e87cfd5f6d1a89
Austin Graham <austiingraham@gmail.com>
2023-12-21 00:42:54 -0500

    renamed database.js to server.js, moved filed from js folder to root directory, initialized inquirer in app.js

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const mysql = require('mysql2');
 const dotenv = require('dotenv');
-const { startApp } = require('./app');
+const { startApp } = require('./js/app');
 
-dotenv.config();
+require('dotenv').config()
 
 const connection = mysql.createConnection({
   host: process.env.DB_HOST,
@@ -13,10 +13,10 @@ const connection = mysql.createConnection({
 
 connection.connect((error) => {
   if (error) {
-    console.error('\x1b[31m Error connecting to the database:\x1b[0m', error.message);
+    console.error('\x1b[31mError connecting to the database:\x1b[0m', error.message);
     return;
   }
 
-  console.log('\x1b[32m Connected to the database\x1b[0m');
+  console.log('\x1b[32mConnected to the database\x1b[0m');
   startApp(connection);
 });
