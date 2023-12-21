2f1e3e943aeb386c3b91ecd9982ee308dddb32b0
Austin Graham <austiingraham@gmail.com>
2023-12-20 23:07:03 -0500

    refactored MySQL connection setup and integrated app startup

--- js/database.js
+++ js/database.js
@@ -1,20 +1,22 @@
 const mysql = require('mysql2');
-require('dotenv').config()
+const dotenv = require('dotenv');
+const { startApp } = require('./app');
+
+dotenv.config();
 
 const connection = mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
-  database: process.env.DB_NAME,
+  database: process.env.DB_NAME
 });
 
-connection.connect((err) => {
-  if (err) {
-    console.error('\x1b[31m Error connecting to database:\x1b[0m', err.message);
+connection.connect((error) => {
+  if (error) {
+    console.error('\x1b[31m Error connecting to the database:\x1b[0m', error.message);
     return;
   }
 
   console.log('\x1b[32m Connected to the database\x1b[0m');
+  startApp(connection);
 });
-
-module.exports = connection;
